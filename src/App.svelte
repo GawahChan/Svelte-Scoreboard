70281f2f356e24517c89be0b93b6588fe8edd02a
Gawah Chan <gawahchan@gmail.com>
2019-09-17 15:14:52 +0100

    Added sorting feature

--- src/App.svelte
+++ src/App.svelte
@@ -27,11 +27,25 @@
   const removePlayer = (e) => {
 	  players = players.filter(player => player.name !== e.detail);
   }
+
+  const sortHighest = () => {
+	  players = players.sort((a, b) => b.points - a.points);
+  }
+
+  const sortLowest = () => {
+	  players = players.sort((a, b) => a.points - b.points);
+  }
 </script>
 
 <Navbar />
 <div class="container"> 
 	<AddPlayer on:addplayer={addPlayer}/>
+	<button class='sort-highest' on:click={sortHighest}>
+		Sort Highest
+	</button>
+		<button class='sort-lowest' on:click={sortLowest}>
+		Sort Lowest
+	</button>
 	{#if players.length === 0}
 		<p>No Players</p>
 	{:else}
